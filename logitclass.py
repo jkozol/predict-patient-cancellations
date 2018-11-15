4b0e650e97defec6af9a71b53372920c80efb244
jkozol <jacobdkozol@gmail.com>
2018-11-14 22:57:23 -0500

    Update logit to use training data

--- logitclass.py
+++ logitclass.py
@@ -4,6 +4,11 @@ import pandas as pd
 import sys
 
 from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+from sklearn.model_selection import train_test_split
+
 
 def loadData():
     dataset = pd.read_csv('data/data.csv')
@@ -11,7 +16,7 @@ def loadData():
     target = 'No Show/LateCancel Flag'
     X = dataset[feature_cols]
     y = dataset[target]
-    print(X,y)
+    return(X, y)
 
 def train(X, y):
     old_stdout = sys.stdout
@@ -27,4 +32,21 @@ def train(X, y):
         loss_list.append(float(line.split("loss: ")[-1]))
     pass
 
-loadData()
+
+def test():
+    dataset = pd.read_csv('data/data.csv')
+    feature_cols = ['Date Diff']
+    target = 'No Show/LateCancel Flag'
+    X = dataset[feature_cols]
+    y = dataset[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    logit = SGDClassifier()
+    logit.fit(X_train, y_train)
+    y_pred = logit.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print(conf_matrix)
+    print(classification_report(y_test, y_pred))
+
+test()
