023bb0d5253c249223c60d8df1865cb417d205ff
jkozol <jacobdkozol@gmail.com>
2018-11-17 19:49:55 -0500

    Update data processing

--- logitclass.py
+++ logitclass.py
@@ -5,7 +5,9 @@ import sys
 from sklearn.linear_model import SGDClassifier,LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 def loadData():
     dataset = pd.read_csv('data/data.csv')
@@ -15,12 +17,27 @@ def loadData():
     y = dataset[target]
     return(X, y)
 
-def trainLogit(X, y):
+def processData(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+    # sc = StandardScaler()
+    # X_train = sc.fit_transform(X_train)
+    # X_test = sc.transform(X_test)
+
+    X_train_bal1 = X_train[y_train == 1]
+    y_train1 = y_train[y_train == 1]
+    X_train_bal0 = X_train[y_train == 0].sample(len(X_train_bal1))
+    y_train0 = y_train[y_train == 0].sample(len(X_train_bal1))
+    print(X_train_bal0.size, X_train_bal1.size)
+
+    X_train = pd.concat([X_train_bal1, X_train_bal0])
+    y_train = pd.concat([y_train1, y_train0])
+    return(X_train, X_test, y_train, y_test)
+
+def trainLogit(X_train, X_test, y_train, y_test):
     logit = LogisticRegression(class_weight='balanced')
-    model = logit.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    logit.fit(X_train, y_train)
+    y_pred = logit.predict(X_test)
 
     conf_matrix = confusion_matrix(y_test, y_pred)
 
@@ -29,13 +46,10 @@ def trainLogit(X, y):
     print(conf_matrix)
     print(classification_report(y_test, y_pred))
 
-def trainForest(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-
-    forest = RandomForestClassifier(n_estimators=100)
-    model = forest.fit(X_train,y_train)
-    y_pred = model.predict(X_test)
+def trainForest(X_train, X_test, y_train, y_test):
+    rf = RandomForestClassifier(n_estimators=100, random_state=0)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
 
     conf_matrix = confusion_matrix(y_test, y_pred)
 
@@ -45,5 +59,6 @@ def trainForest(X, y):
     print(classification_report(y_test, y_pred))
 
 X, y = loadData()
-trainLogit(X, y)
-trainForest(X, y)
+X_train, X_test, y_train, y_test = processData(X, y)
+trainLogit(X_train, X_test, y_train, y_test)
+trainForest(X_train, X_test, y_train, y_test)
