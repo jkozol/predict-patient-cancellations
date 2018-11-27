abc46ca538f363d34d55935fc31714fadbba82b3
Nick Pearce <npearce@bu.edu>
2018-11-27 17:02:04 -0500

    Update dataview

--- dataview.py
+++ dataview.py
@@ -8,19 +8,21 @@ def age(b):
 # Load csvs into dataframes
 enrollment_df = pd.read_csv("data/enrollment.csv")
 train_df = pd.read_csv("data/train.csv")
+events_df = pd.read_csv("data/events.csv")
 
-# Drop unnecessary columns from training data
-train_df.drop(columns=['Registration Date', 'Procedure Date'], inplace=True)
+# Drop unnecessary columns from training and events data
+train_df.drop(columns=['Index Id', 'Hospital Id', 'Registration Date', 'Procedure Date'], inplace=True)
+events_df.drop(columns=['Hospital Id', 'Event_Date', 'Event_Time', 'Event_Name', 'Event_Desc'], inplace=True)
 
-#Compute difference (in days) between Registration Date and Procedure Date
+# Compute difference (in days) between Registration Date and Procedure Date
 enrollment_df['Registration Date'] = pd.to_datetime(enrollment_df['Registration Date'])
 enrollment_df['Procedure Date'] = pd.to_datetime(enrollment_df['Procedure Date'])
 enrollment_df['Date Diff'] = enrollment_df.apply(lambda row: (row['Procedure Date'] - row['Registration Date']).days, axis=1)
-#Convert boolean strings to integer values
+# Convert boolean strings to integer values
 enrollment_df['Email'] = enrollment_df['Email'].apply(lambda x: x*1)
 enrollment_df['SMS'] = enrollment_df['SMS'].apply(lambda x: x*1)
 enrollment_df['Gender'] = enrollment_df['Gender'].apply(lambda x: x == 'Male').astype(int)
-#Compute age from date of birth
+# Compute age from date of birth
 enrollment_df['Date of Birth'] = pd.to_datetime(enrollment_df['Date of Birth'])
 enrollment_df['Age'] = enrollment_df['Date of Birth'].apply(lambda x: age(x))
 # Drop unnecessary columns from enrollment data
@@ -28,7 +30,32 @@ enrollment_df.drop(columns=['Hospital Id', 'Registration Date', 'Procedure Date'
 # Drop rows with NaN
 enrollment_df.dropna(how='any', inplace=True)
 
-# Combine training and enrollment data
+# Aggregate events data
+grouped_module = events_df.groupby(['Patient Id', 'Module Id'])
+grouped_message = events_df.groupby(['Patient Id', 'Message Id'])
+# Enumerate unique Patient Ids, Module Ids, and Message Ids
+patients = enrollment_df['Patient Id'].unique()
+modules = events_df['Module Id'].unique().astype(str)[1:]
+messages = events_df['Message Id'].unique().astype(str)[1:]
+# Construct additional dataframes, populate with module and message counts
+module_df = pd.DataFrame(0, index=patients, columns=modules)
+module_df.index.name = 'Patient Id'
+message_df = pd.DataFrame(0, index=patients, columns=messages)
+message_df.index.name = 'Patient Id'
+for name, group in grouped_module:
+    patient = name[0]
+    module = name[1]
+    if patient in patients:
+        module_df.at[patient, str(module)] = group.size
+for name, group in grouped_message:
+    patient = name[0]
+    message = name[1]
+    if patient in patients:
+        message_df.at[patient, str(message)] = group.size
+        
+# Combine training, events, and enrollment data
+enrollment_df = enrollment_df.merge(module_df, on='Patient Id')
+enrollment_df = enrollment_df.merge(message_df, on='Patient Id')
 result = enrollment_df.merge(train_df.set_index('Patient Id'), on='Patient Id',)
 
 print(result.head())
