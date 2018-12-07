feec5ce11e3773bc752534825ed9c89f20f4c8af
shuheather <45043892+shuheather@users.noreply.github.com>
2018-12-06 19:58:43 -0500

    Update SVMclass.py

--- SVMclass.py
+++ SVMclass.py
@@ -1,27 +1,56 @@
-from logitclass import loadData, processData, trainForest
+import pandas as pd
+import numpy as np
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import KFold
+
+def loadData():
+    dataset = pd.read_csv('data/data_train.csv')
+    #feature_cols = ['Date Diff', 'SMS', 'Email', 'Gender', 'Age']
+    #target = 'No Show/LateCancel Flag'
+    X = dataset.drop(columns=['Patient Id', 'No Show/LateCancel Flag'])
+    y = dataset['No Show/LateCancel Flag']
+    return(X, y)
+
 
 def trainSVM(X_train, X_test, y_train, y_test, krnl):
     svm = SVC(kernel = krnl)
     svm.fit(X_train, y_train)
     y_pred = svm.predict(X_test)
+    auc = roc_auc_score(y_test, y_pred)
+    return (auc, svm)
+
+def SVM_KFoldValidation(X, y, func, k, krnl):
+    model = None
+    max_auc = 0.0
+    aucs = np.array([])
+    kfold = KFold(X.shape[0], n_folds=k)
     
-    conf_matrix = confusion_matrix(y_test, y_pred)
+    for train_index, test_index in kfold:
+        print("here1")
+        X_train = X.iloc[train_index]
+        X_test = X.iloc[test_index]
+        y_train = y.iloc[train_index]
+        y_test = y.iloc[test_index]
+        print("here2")
+        auc, mod = func(X_train, X_test, y_train, y_test, krnl)
+        print("here3")
+        aucs = np.append(aucs, auc)
+        if auc > max_auc:
+            model = mod
+            max_auc = auc
+            
+    return (np.mean(aucs), model)
 
-    print("Support Vector Machine with", krnl, "kernel")
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print(conf_matrix)
-    print(classification_report(y_test, y_pred))
-
-
-X, y = loadData()
-X_train, X_test, y_train, y_test = processData(X, y)
-
-trainSVM(X_train, X_test, y_train, y_test, 'linear')
-trainSVM(X_train, X_test, y_train, y_test, 'rbf')
-trainSVM(X_train, X_test, y_train, y_test, 'sigmoid')
-trainSVM(X_train, X_test, y_train, y_test, 'poly')
-trainForest(X_train, X_test, y_train, y_test)
+X,y = loadData()
+auc_svml, svml = kFoldValidation(X, y, trainSVM, 10, 'linear')
+print("Linear SVM AUC: ", auc_svml)
+auc_svmr, svmr = kFoldValidation(X, y, trainSVM, 10, 'rbf')
+print("RBF SVM AUC: ", auc_svmr)
+auc_svms, svms = kFoldValidation(X, y, trainSVM, 10, 'sigmoid')
+print("Sigmoid SVM AUC: ", auc_svms)
+auc_svmp, svmp = SVM_KFoldValidation(X, y, trainSVM, 10, 'poly')
+print("Polynomial SVM AUC: ", auc_svmp)
 
 
